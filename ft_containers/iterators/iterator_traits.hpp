06022e35d8b1004af00ade2c94efef7cc2284300
Jongseok Gim <jgim@c3r8s5.42seoul.kr>
2022-05-24 15:53:48 +0900

    container

---
+++ ft_containers/iterators/iterator_traits.hpp
@@ -0,0 +1,17 @@
+#ifndef ITERATOR_TRAITS_HPP
+# define ITERATOR_TRAITS_HPP
+
+namespace ft
+{
+	template <class Iterator>
+
+	struct iterator_traits {
+	typedef typename Iterator::iterator_category iterator_category;
+	typedef typename Iterator::value_type value_type;
+	typedef typename Iterator::difference_type difference_type;
+	typedef difference_type distance_type;
+	typedef typename Iterator::pointer pointer;
+	typedef typename Iterator::reference reference;
+   };
+}
+   #endif
